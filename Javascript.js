f8ca3dbd4d0adabf65fe98d2b9d0d4da2abca831
LuciaForneron <49293880+LuciaForneron@users.noreply.github.com>
2019-08-07 06:11:23 -0300

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+Frases = new Array();
+Frases[0] = "I'm just your average, run-of-the-mill Russian bisexual transvestite hooker. -Katya" ;
+Frases[1] = "Katya is at the intersection of glamour and comedy. You can find her right on the corner, selling her ass. -Katya";
+Frases[2] = "You know what you can suck? My whole dick. -Katya";
+Frases[3] = "I love love. Every day of my life is Valentine's Day. When you're a pathological narcissist, you have to fall in love with yourself every day. -Katya";
+Frases[4] = "What does not kill you gives you a lot of unhealthy coping mechanisms and a really dark sense of humor. -Katya";
+Frases[5] = "I don't have diabetes, I've never had diabetes, but if I wanted to get diabetes I could, but you can't have diabetes cuz you're not that kind of high fructose girl. -Katya";
+number = Math.round(Math.random()*4);
+document.write(Frases[number]);
